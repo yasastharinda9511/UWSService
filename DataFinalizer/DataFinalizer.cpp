098f62f1dd3093e3d718debd0517db8960bb1537
yasas <yasas.tharinda@axiatadigitallabs.com>
2024-09-01 09:27:36 +0530

    refactor the codes

--- DataFinalizer/DataFinalizer.cpp
+++ DataFinalizer/DataFinalizer.cpp
@@ -9,47 +9,43 @@
 #include "DataFinalizeHandlers/GetAllStudentsResponseHandler.h"
 
 void Finalizer::DataFinalizer::listen(Basics::BaseTraveller &base_data_traveller) {
-    auto data_traveller = dynamic_cast<Basics::DataTraveller<false>*>(&base_data_traveller);
+    auto data_traveller = dynamic_cast<Basics::DataTraveller<false> *>(&base_data_traveller);
 
-    if(data_traveller != nullptr){
+    if (data_traveller != nullptr) {
         auto ite = _response_mappers.find(base_data_traveller.get_event());
-        if(base_data_traveller.get_error_code() != 200) {
-            _loop->defer([this, data_traveller](){
-                std::string response_json = _response_mappers.find(Constants::Events::ERROR)->second->handel_response(*data_traveller);
+        if (base_data_traveller.get_error_code() != 200) {
+            _loop->defer([this, data_traveller]() {
+                std::string response_json = _response_mappers.find(Constants::Events::ERROR)->second->handel_response(
+                        *data_traveller);
                 data_traveller->get_response()->end(response_json);
                 data_traveller->get_response()->writeHeader("Connection", "keep-alive");
                 delete data_traveller;
                 return;
             });
-
-
         }
 
-        if(ite != _response_mappers.end()){
+        if (ite != _response_mappers.end()) {
 
-            if(_loop != nullptr){
-                std::cout << "yasas tharinda !!!" << std::endl;
+            if (_loop != nullptr) {
+
+                _loop->defer([&]() {
+                    std::string response_json = ite->second->handel_response(base_data_traveller);
+                    data_traveller->get_response()->writeHeader("Connection", "keep-alive");
+                    data_traveller->get_response()->end(response_json);
+                });
             }
-
-//
-            _loop->defer([&]() {
-                auto response = data_traveller->get_response();
-                std::cout << "yasas tharinda 234567!!!" << std::endl;
-                auto x = ite->first;
-                std::string response_json = ite->second->handel_response(base_data_traveller);
-                data_traveller->get_response()->writeHeader("Connection", "keep-alive");
-                data_traveller->get_response()->end(response_json);
-            });
         }
 
         //delete data_traveller;
     }
 }
 
-Finalizer::DataFinalizer::DataFinalizer(uWS::Loop *loop) : _loop(loop){
+Finalizer::DataFinalizer::DataFinalizer(uWS::Loop *loop) : _loop(loop) {
 
-    _response_mappers.try_emplace(Constants::Events::ADD_STUDENT, new Finalizer::ResponseMapHandlers::AddStudentResponseHandler());
-    _response_mappers.try_emplace(Constants::Events::GET_ALL_STUDENTS, new Finalizer::ResponseMapHandlers::GetAllStudentsResponseHandler());
+    _response_mappers.try_emplace(Constants::Events::ADD_STUDENT,
+                                  new Finalizer::ResponseMapHandlers::AddStudentResponseHandler());
+    _response_mappers.try_emplace(Constants::Events::GET_ALL_STUDENTS,
+                                  new Finalizer::ResponseMapHandlers::GetAllStudentsResponseHandler());
     _response_mappers.try_emplace(Constants::Events::ERROR, new Finalizer::ResponseMapHandlers::ErrorHandler());
 }
 
